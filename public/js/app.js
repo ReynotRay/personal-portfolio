88a7a77f26bf85a90609eb6429f540270df9aced
Rey Garcia <reynaldogarcia@Reynaldos-iMac.local>
2016-10-11 01:20:59 -0600

    refactor

--- public/js/app.js
+++ public/js/app.js
@@ -6,17 +6,29 @@ var Portfolio = function() {
 };
 
 Portfolio.prototype.mouseenter = function(event) {
-$( ".thumbnail" ).mouseenter(function() {
+	$( ".thumbnail" ).mouseenter(function() {
   	$(this).children().fadeIn();
-    $(this).css("filter", "saturate(5)");
+    $(this).css("filter", "saturate(2)");
   })
   .mouseleave(function() {
   	$(this).children().hide();
     $(this,'img').css("filter", "saturate(1)");
   });
+  portfolio.showSnapChat();
+}
+
+Portfolio.prototype.showSnapChat = function(event) {
+	$( "#first-snapchat" ).click(function() {
+	$('#first-snapchat').css('display','none');
+	$('#snapchat').fadeIn();
+  	$( "li" ).mouseleave(function() {
+  	$('#snapchat').hide();
+  	$('#first-snapchat').fadeIn();
+    });
+})
+
 }
 
 var portfolio = new Portfolio();
 portfolio.mouseenter();
-
 
