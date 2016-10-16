8d3a8c5611803f7df1651018c8274d05d8445762
ReynotRay <reygarcia718@gmail.com>
2016-10-16 00:33:57 -0600

    working

--- public/js/newapp.js
+++ public/js/newapp.js
@@ -4,10 +4,11 @@ var Portfolio = function() {
     // this.input = $('.list-todo');
     // this.list = $('.todo-items');
     // $(this).css("filter", "saturate(2)");
+    this.story = $("#my-story");
 };
 
 Portfolio.prototype.mouseenter = function(event) {
-    $("#my-story").mouseenter(function() {
+    this.story.mouseenter(function() {
             $("#my-projects").css({"-webkit-transition": ".9s", "-webkit-clip-path": "polygon(1125px 0, 998px 0, 987px 1020px, 767px 645px)"});
             $(this).css({"-webkit-transition": ".9s", "-webkit-clip-path": "polygon(0 0, 0 0, 0 1999px, 1143px 0)"});
         })  
@@ -16,12 +17,12 @@ Portfolio.prototype.mouseenter = function(event) {
             $('#my-projects').css("-webkit-clip-path", "polygon(746px 0, 991px 0, 990px 613px, 240px 613px)");
         });
     $("#my-projects").mouseenter(function() {
-            $('#my-story').css("-webkit-clip-path", "polygon(0 0, 0 0, 0 309px, 304px 0)");
+            $("#my-story").css("-webkit-clip-path", "polygon(0 0, 0 0, 0 309px, 304px 0)");
             $('#my-projects').css("-webkit-clip-path", "polygon(220px 0, 999px 0, 993px 613px, -248px 612px)");
         })
         .mouseleave(function() {
-          $('#my-story').css("-webkit-clip-path", "polygon(0 0, 0 0, 0 934px, 750px 0)");
-          $('#my-projects').css("-webkit-clip-path", "polygon(746px 0, 991px 0, 990px 613px, 240px 613px)");
+            $("#my-story").css("-webkit-clip-path", "polygon(0 0, 0 0, 0 934px, 750px 0)");
+            $('#my-projects').css("-webkit-clip-path", "polygon(746px 0, 991px 0, 990px 613px, 240px 613px)");
             console.log('left-projects');
         });
 }
